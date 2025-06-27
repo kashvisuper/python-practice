ee9b1b3afe775154a08daf78ed4d41baa86cbdc7
kashvisuper <kashvi.gupta0896@gmail.com>
2025-06-28 05:25:39 +0530

    add madlibs project

---
+++ main3.py
@@ -0,0 +1,12 @@
+#madlibs game
+#word game to create a story by filling random words
+adjective1=input("enter adjective")
+noun1=input("enter noun")
+adjective2=input("enter adjective")
+verb1=input("enter verb")
+adjective3=input("enter adjective")
+
+print(f"Today i went to a {adjective1}zoo.")
+print(f"in an exhibit , i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
